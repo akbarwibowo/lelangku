adc86789c17f8b55fc182cd77c4e9f69b9d64980
akbar_wibowo <akbarwibowo0123@gmail.com>
2023-09-01 12:42:40 +0700

    initial commit

---
+++ app/src/main/java/com/example/lelangku/ui/theme/Dashboard.kt
@@ -0,0 +1,12 @@
+package com.example.lelangku.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lelangku.R
+
+class Dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
